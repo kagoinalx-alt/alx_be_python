fafd0e3de8f7500e4a2563406ce54020ea679e05
Robert Kago <kagoinalx@gmail.com>
2025-12-09 11:00:18 +0300

    Modified fns_and_dsa

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -10,7 +10,7 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 return "Error: Division by zero is not allowed."
-            if num1 == 0:
+            elif num1 == 0:
                 return "Error: Division by zero is not allowed."
             else:
                 return num1 / num2
