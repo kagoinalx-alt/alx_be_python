316472ece5e0a17d175734bff8c43d3dfd0a7f87
Robert Kago <kagoinalx@gmail.com>
2025-12-09 17:22:55 +0300

    Committed shopping_list script

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,38 @@
+# Implement functionality to add items to the list, remove items, and display the current list.
+def display_menu():
+    print("Shooping list manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+        if choice == '1':
+            item = input("Ener the item you would like to add: ")
+            shopping_list.append(item)
+            print(f"{item}, has been added to the list.")
+        elif choice == '2':
+            item = input("Enter the item you would like to remove: ")
+            if item in shopping_list:
+                shopping_list.remove(item)
+                print(f"{item}, has been removed from the list.")
+            else:
+                print(f"{item}, is not in the list.")
+        elif choice == '3':
+            print("Current shopping list:")
+            for index, item in enumerate(shopping_list, start=1):
+                print(f"{index}. {item}")
+        elif choice == '4':
+            print("Goodbye.")
+            break
+        else:
+            print("Invalid choice, please try again.")
+
+
+if __name__ == "__main__":
+    main()
