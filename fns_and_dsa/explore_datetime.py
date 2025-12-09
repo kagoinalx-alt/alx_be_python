74e06edd1b635f1fcc685383b1bf92b874a1d4ed
Robert Kago <kagoinalx@gmail.com>
2025-12-09 23:46:57 +0300

    added input line to the file

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -9,9 +9,6 @@ if __name__ == "__main__":
     display_current_datetime()
 
 
-days = int(input("Enter number of days to add to the current date:"))
-
-
 def calculate_future_date(days_to_add):
     from datetime import datetime, timedelta
     current_date = datetime.now()
