fda82b87700d84e4ad294fd9a3466abe66a54487
Robert Kago <kagoinalx@gmail.com>
2025-12-09 23:39:35 +0300

    Modified the future_date file

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,7 +1,8 @@
 def display_current_datetime():
     from datetime import datetime
     current_date = datetime.now()
-    print(f"Current date and time: {current_date.strftime('%Y-%m-%d')}")
+    print(
+        f"Current date and time: {current_date.strftime('%Y-%m-%d %H:%M:%S')}")
 
 
 if __name__ == "__main__":
