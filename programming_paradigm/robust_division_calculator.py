e9fa53403218998c7f208adb57d730c08827d867
Robert Kago <kagoinalx@gmail.com>
2026-01-05 15:12:30 +0300

    Committed robust calculator file

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,13 @@
+def safe_divide(numerator, denominator):
+    try:
+        num = float(numerator)
+        den = float(denominator)
+
+        result = num / den
+        return result
+
+    except ZeroDivisionError:
+        return "Error: Division by zero is not allowed."
+    except ValueError:
+        return "Error: Please enter numeric values."
+    
