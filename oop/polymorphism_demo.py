69c854562406b447b3d0a857e87cf8d26680bb51
Robert Kago <kagoinalx@gmail.com>
2026-01-05 18:47:00 +0300

    First Commit of polymorphism demo file

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,21 @@
+class Shape:
+    def area(self):
+        raise NotImplementedError("Subclasses need to override this method")
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return f" The area of the Rectangle is: {self.length * self.width}"
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        import math
+        return f"The area of the Circle is:{math.pi * (self.radius ** 2)}"
