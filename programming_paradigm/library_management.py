a7ce56d44f33aba1df3b2164fc530d4b99641cc6
Robert Kago <kagoinalx@gmail.com>
2026-01-05 15:44:32 +0300

    Modified the library file

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,25 +1,43 @@
 class Book:
-    def __init__(self, title, author, _is_checked_out):
+    def __init__(self, title, author, _is_checked_out=False):
         self.title = title
         self.author = author
         self.__is_checked_out = _is_checked_out
 
+    def check_out(self):
+        if not self.__is_checked_out:
+            self.__is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        if self.__is_checked_out:
+            self.__is_checked_out = False
+            return True
+        return False
+
+    def is_available(self):
+        return not self.__is_checked_out
+
+
 class Library:
     def __init__(self):
         self.__books = []
+
     def add_book(self, book):
         self.__books.append(book)
+
     def check_out_book(self, title):
         for book in self.__books:
-            if book.title == title and not book._Book__is_checked_out:
-                book._Book__is_checked_out = True
-                return True
+            if book.title == title:
+                return book.check_out()
         return False
+
     def return_book(self, title):
         for book in self.__books:
-            if book.title == title and book._Book__is_checked_out:
-                book._Book__is_checked_out = False
-                return True
+            if book.title == title:
+                return book.return_book()
         return False
+
     def list_available_books(self):
-        return [book.title for book in self.__books if not book._Book__is_checked_out]
+        return [book.title for book in self.__books if book.is_available()]
