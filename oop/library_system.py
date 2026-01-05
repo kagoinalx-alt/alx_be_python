71a3e1f2114388a2b9aad699a1a734cc8612d94a
Robert Kago <kagoinalx@gmail.com>
2026-01-05 18:06:57 +0300

    Modified  output method

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,23 +4,25 @@ class Book:
         self.author = author
 
     def __str__(self):
-        return f"{self.title} by {self.author}"
+        return f"Book: {self.title} by {self.author}"
 
-    class EBook(Book):
-        def __init__(self, file_size):
-            super().__init__(self.title, self.author)
-            self.file_size = file_size
 
-        def __str__(self):
-            return f"{self.title} by {self.author}, File Size: {self.file_size}MB"
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
 
-    class PrintBook(Book):
-        def __init__(self, page_count):
-            super().__init__(self.title, self.author)
-            self.page_count = page_count
+    def __str__(self):
+        return f"EBook:{self.title} by {self.author}, File Size: {self.file_size}KB"
 
-        def __str__(self):
-            return f"{self.title} by {self.author}, Pages: {self.page_count}"
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
 class Library:
@@ -32,4 +34,4 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            print(f"{book.title} by {book.author}")
+            print(book.__str__())
